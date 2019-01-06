a3d4f00e2f5cd4ed81f585479b9c99516921af1e
el-ethan <elethan@gmail.com>
2019-01-06 14:26:50 -0600

    Adding js and pre-commit file

---
+++ pre-commit.js
@@ -0,0 +1,42 @@
+const { exec } = require('child-process-promise');
+const process = require('process');
+
+const linterName = 'eslint';
+
+const filesToOmitFromLinting = [
+    'my-minified-file.js',
+    'my-file-to-ignore.js'
+];
+
+async function getFilesToLint() {
+    const filesToLint = await exec('git diff HEAD --name-only').then(({stdout}) => {
+        const files = stdout.split('\n');
+        const filesToLint = files.filter(thisFile => {
+            return thisFile.endsWith('.js') && !filesToOmitFromLinting.includes(thisFile);
+        });
+        return filesToLint;
+    });
+    return filesToLint;
+}
+
+async function lintFiles() {
+    const filesToLint = await getFilesToLint();
+    return exec(`${linterName} ${filesToLint.join(' ')}`)
+        .then(({stdout}) => {
+            return {exitCode: 0, output: stdout};
+        })
+        .catch(({stdout}) => {
+            return {exitCode: 1, output:stdout};
+        });
+}
+
+async function main() {
+    const results = await lintFiles();
+    if (results.exitCode === 1 && results.output) {
+        console.log(results.output);
+        console.log('You have some linting errors. Please see the output above and fix the errors before commiting, our ignore errors and commit using `git commit --no-verify`');
+    }
+    process.exit(results.exitCode);
+}
+
+main();
