66bb9d4db7813c5ca9318645c37553b369ce51ec
el-ethan <elethan@gmail.com>
2019-01-06 20:53:54 -0600

    Reorganize

--- index.js
+++ index.js
@@ -9,6 +9,29 @@ const preCommitFileContent = '#!/bin/sh\n\nlint-trap --check\n\nexit $?\n';
 const gitPath = './.git/';
 
 
+function main() {
+    program
+        .option('-c, --check', 'Check js files with linter')
+        .option('-i, --init', 'Initialize git repo for use with pre-commit-lint')
+        .option('-r, --remove', 'Don\'t track this repo with pre-commit-lint any more')
+        .parse(process.argv);
+
+
+    if (program.init) {
+        const needsPreCommitHook = isGitRepo() && !hasPreCommitHook();
+        if (needsPreCommitHook) {
+            createPreCommitHook();
+            console.log(`pre-commit hook file created at ${preCommitFilePath}`);
+        } else {
+            console.log('pre-commit file already exists.');
+        }
+    } else if (program.check) {
+        lintFiles();
+    }
+}
+main();
+
+
 function isGitRepo() {
     try {
         return fs.lstatSync(gitPath).isDirectory();
@@ -38,28 +61,3 @@ function createPreCommitHook() {
     // Make file executable by Git
     fs.chmodSync(preCommitFilePath, '775');
 }
-
-
-function main() {
-    program
-        .option('-c, --check', 'Check js files with linter')
-        .option('-i, --init', 'Initialize git repo for use with pre-commit-lint')
-        .option('-r, --remove', 'Don\'t track this repo with pre-commit-lint any more')
-        .parse(process.argv);
-
-
-    if (program.init) {
-        const needsPreCommitHook = isGitRepo() && !hasPreCommitHook();
-        if (needsPreCommitHook) {
-            createPreCommitHook();
-            console.log(`pre-commit hook file created at ${preCommitFilePath}`);
-        } else {
-            console.log('pre-commit file already exists.');
-        }
-    } else if (program.check) {
-        lintFiles();
-    }
-}
-
-
-main();
