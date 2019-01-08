d5a7be2e84bea9719c353ae1cb0dc4dd5329c788
el-ethan <elethan@gmail.com>
2019-01-07 21:47:58 -0600

    Use optional config

--- pre-commit.js
+++ pre-commit.js
@@ -1,12 +1,19 @@
 const { exec } = require('child-process-promise');
 const process = require('process');
 
-const linterName = 'eslint';
+const defaultConfig = {
+    filesToIgnore: [],
+    linter: 'eslint'
+};
 
-const filesToOmitFromLinting = [
-    'my-minified-file.js',
-    'my-file-to-ignore.js'
-];
+let userConfig;
+try {
+    const configFromFile = require('./.pc-lint-config.json');
+    userConfig = Object.assign(defaultConfig, configFromFile);
+} catch(error) {
+    userConfig = defaultConfig;
+}
+
 
 async function main() {
     const results = await lintFiles();
@@ -19,20 +26,22 @@ or ignore errors and commit using \`git commit --no-verify\`.`);
     process.exit(results.exitCode);
 }
 
+
 async function getFilesToLint() {
     const filesToLint = await exec('git diff HEAD --name-only').then(({stdout}) => {
         const files = stdout.split('\n');
         const filesToLint = files.filter(thisFile => {
-            return thisFile.endsWith('.js') && !filesToOmitFromLinting.includes(thisFile);
+            return thisFile.endsWith('.js') && !userConfig.filesToIgnore.includes(thisFile);
         });
         return filesToLint;
     });
     return filesToLint;
 }
 
+
 async function lintFiles() {
     const filesToLint = await getFilesToLint();
-    return exec(`${linterName} ${filesToLint.join(' ')}`)
+    return exec(`${userConfig.linter} ${filesToLint.join(' ')}`)
         .then(({stdout}) => {
             return {exitCode: 0, output: stdout};
         })
@@ -40,5 +49,6 @@ async function lintFiles() {
             return {exitCode: 1, output:stdout};
         });
 }
+
 
 module.exports = main;
